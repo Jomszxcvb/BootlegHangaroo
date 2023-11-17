d2fb6454739806e6aa80c8eec385f2ddc361b739
Jomszxcvb <127298158+Jomszxcvb@users.noreply.github.com>
2023-11-17 07:21:49 +0000

    Refactor Stage class and add playStage method

--- src/Stage.java
+++ src/Stage.java
@@ -1,50 +1,59 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Stage {
-    private WordGenerator wordGenerator = new WordGenerator();
-    private List<Word> words;
+    private WordGenerator wordGenerator;
+    private Word word;
+    private static int stageNumber;
 
-    private static int stageNumber = 1;
-
-    public Stage(Player player, GameMode gameMode){
-        words = wordGenerator.generateWords(gameMode);
-        while (player.getHealth() != Player.NO_HEALTH && !words.get(stageNumber).isGuessed()) {
-            play();
-
-            if (words.get(stageNumber).guessLetter(player.guess())) {
-                System.out.println("Correct!");
-                player.incScore();
-            } else {
-                System.out.println("Wrong!");
-                player.decHealth();
-            }
-
-            if (words.get(stageNumber).isGuessed()) {
-                System.out.println("You guessed the word!");
-                player.incToken();
-                stageNumber++;
-            }
-        }
+    Stage(GameMode gameMode){
+        wordGenerator = new WordGenerator(gameMode);
+        word = wordGenerator.generateWord();
+        stageNumber = 1;
     }
 
-    public void play(){
-        System.out.println("Stage " + stageNumber);
-        System.out.println("Health: " + Player.MAX_HEALTH);
-        System.out.println("Score: " + Player.MIN_SCORE);
-        System.out.println("Token: " + Player.MIN_TOKEN);
-        System.out.println(words.get(stageNumber).retrieveGuessedLetters());
-        System.out.println("Guess a letter: ");
+    public static int getStageNumber() {
+        return stageNumber;
+    }
+
+    public void playStage(Player player) {
+        while (player.getHealth() != Player.NO_HEALTH) {
+            if (!word.isGuessed()) {
+                System.out.println("Stage " + stageNumber);
+                player.displayPlayerInfo();
+                System.out.println("Guess the word: ");
+                System.out.println(word.retrieveGuessedLetters());
+                System.out.println("Enter your guess: ");
+                if (word.guessLetter(player.getChar())) {
+                    System.out.println("Correct!");
+                }
+                else {
+                    System.out.println("Wrong!");
+                    player.decHealth();
+                }
+            }
+            else {
+                System.out.println("You guessed the word!");
+                System.out.println("The word is: " + word.getWord());
+                player.incScore();
+                stageNumber++;
+                word = wordGenerator.generateWord();
+            }
+        }
+
+
     }
 
     /**
      * TEST CODE
     public static void main(String[] args) {
-        Player player = new Player();
-        Stage stage = new Stage(player, GameMode.CLASSIC);
-        stage.play();
+        Player player = new Player("Jom");
+        Stage stage = new Stage(GameMode.CLASSIC);
+        stage.playStage(player);
     }
     */
 }
+
 
 
 
